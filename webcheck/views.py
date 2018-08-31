dbbbcf6824bf707b2bc4adb28b0bfa0de9bd95a1
Pranay Rai <40250685+pranay-rai@users.noreply.github.com>
2018-08-31 15:32:52 -0700

    update from last day

--- webcheck/views.py
+++ webcheck/views.py
@@ -1,13 +1,6 @@
 import time
-
-import nmap as nmap
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
-import urllib.request
 import ssl, socket
-
 import requests
 import datetime
 import subprocess
@@ -16,10 +9,12 @@ import tld
 import nmap
 import whois
 
-
-
 from webcheck.models import URLForm
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
+
+# entity is an object created which contains all the details regarding the url as its attributes
 
 class Entity():
     def __init__(self, name, status, validity, expiry, expiryDays, protocols, cipherSuites, reputation, TLDs, openPorts, IPaddr, owner, heartbeat, poodle):
@@ -38,24 +33,31 @@ class Entity():
         self.heartbeat = heartbeat
         self.poodle = poodle
 
-
+# home : for the home page
 def home(request):
     form = URLForm()
     return render(request, 'webcheck\home.html', {'form':form})
 
+#results : for the results page
 def results(request):
 
     if request.method == "POST":
         dataForm = URLForm(request.POST)
-
+        validator = URLValidator()
         if dataForm.is_valid():
             url = dataForm.cleaned_data['url']
+            try:
+                validator(url)
+            except ValidationError:
+                print('Invalid Input')
+
         else:
             dataForm = URLForm()
 
-    start = time.time()
-    def checkStatus(url):
+    start = time.time() # start scan timer
 
+    # function to check the site status - Live ,Timed-Out - Invalid. Uses the response code to determine
+    def checkStatus(url):
         status=''
         try:
             headers = {
@@ -120,6 +122,8 @@ def results(request):
             days = "Could not retrieve"
         return days
 
+
+    # parsed through the command output. Type the command on the subprocess in CMD incase you want to see the output
     def findSupportedProtocols(url):
         protocols = []
         try:
@@ -130,29 +134,30 @@ def results(request):
                     if i.__contains__("Protocol"):
                         continue
                     else:
-                        protocols.append('SSL3 ')
+                        protocols.append('<font color ="#FF0000"> SSL3 </font>')
                 if i.__contains__('TLSv10'):
                     if i.__contains__("Protocol"):
                         continue
                     else:
-                        protocols.append('TLS1.0 ')
+                        protocols.append('<font color ="#FF0000">TLS1.0 </font>')
                 if i.__contains__('TLSv11'):
                     if i.__contains__("Protocol"):
                         continue
                     else:
-                        protocols.append('TLS1.1 ')
+                        protocols.append('<font color ="#008000"> TLS1.1</font> ')
                 if i.__contains__('TLSv12'):
                     if i.__contains__("Protocol"):
                         continue
                     else:
-                        protocols.append('TLS1.2 ')
+                        protocols.append('<font color ="#008000">TLS1.2 </font>')
         except:
             protocols = 'Could Not Fetch'
 
         return ''.join(protocols)
 
+    # parsed through the command output. Type the command on the subprocess in CMD incase you want to see the output. Only the required
+    # words have been parsed from the command line
     def findSupportedCipherSuites(url):
-
         result = []
         try:
             hostname = url.replace("https://", "")
@@ -163,31 +168,39 @@ def results(request):
                     word1 = words[1]
                     word2 = words[4]
                     word3 = words[6]
-                    result.append(word1[word1.index('m')+1:word1.index('m')+7] + " -- " + word2 + " -- " +  word3[4:word3.rfind('\\')-3])
+                    if(word1[word1.index('m')+1:word1.index('m')+7]=='TLSv10' or word1[word1.index('m')+1:word1.index('m')+7] =='SSLv3' or word1[word1.index('m')+1:word1.index('m')+7]=='SSLv2' or word3.__contains__('AES')==False):
+                        result.append('<font color="#FF0000">' + word1[word1.index('m')+1:word1.index('m')+7] + " -- " + word2 + " -- " +  word3[4:word3.rfind('\\')-3] + '</font>')
+                    else:
+                        result.append('<font color="#008000">' + word1[word1.index('m') + 1:word1.index('m') + 7] + " -- " + word2 + " -- " + word3[4:word3.rfind('\\') - 3] + '</font>')
         except:
             result = 'Could not fetch'
 
         return result
+
+    # this function checks the name of the url against the file which contains the list of all malicious websites. It uses regex
+    # to match against the urls
 
     def checkReputation(url):
         reputation = ''
         try:
             res = tld.get_tld(url, as_object=True)
             hostname = res.domain
-
+            filepath = 'C:\\Users\\PRRAI\\PycharmProjects\\portfolio\\jobs\\names.csv' # Enter the file path of the malicious website list
             website_list=[]
-            with open('C:\\Users\\PRRAI\\PycharmProjects\\portfolio\\jobs\\names.csv', 'r') as f:
+            with open(filepath, 'r') as f:
                 website_list = f.readlines()
             r = re.compile('^.*' + hostname + '.*$')
             newList = list(filter(r.match, website_list))
             if len(newList) !=0:
-                reputation = 'Malicious'
+                reputation = 'Believed to be unsafe'
             else:
-                reputation = 'Safe'
+                reputation = 'Believed to be safe'
         except:
             reputation = 'Could not fetch'
         return reputation
 
+    # this function is used to find other Top Level Domains with the same host name. The function strips off the TLD, and
+    # attaches other TLDs from the list and tries to establish a connection. If the connection is established, that is one other TLD.
     def findOtherTLDs(url):
         TLDs = ['ca', 'com', 'uk']
         websites = []
@@ -203,7 +216,12 @@ def results(request):
                         link = 'http://' + res.domain + '.' + domain
                         r = requests.get(link, headers=headers, allow_redirects=False, timeout = 5)
                         if(r.status_code>=200 or r.status_code<=399):
-                            websites.append(link.replace('http://', ''))
+                            if(checkReputation(link)=='Believed to be safe'):
+                                websites.append('<a href=' + link + '><font color = "#008000">' + link.replace('http://', '') + '</font></a>')
+                                continue
+                            elif(checkReputation(link)=='Believed to be unsafe'):
+                                websites.append('<a href=' + link + '><font color = "#FF0000">' + link.replace('http://', '') + '</font></a>')
+                                continue
                         else:
                             continue
                     except:
@@ -215,6 +233,7 @@ def results(request):
 
         return websites
 
+    # This function uses nmap to detect the ports. Modify the flag in arguments as per the requirements as on what range of ports you want to scan
     def findOpenPorts(url):
         result = []
         try:
@@ -223,17 +242,17 @@ def results(request):
             try:
                 ip_addr = socket.gethostbyname(domain)
                 nm = nmap.PortScanner()
-                nm.scan(hosts=ip_addr, arguments='-F')
+                nm.scan(hosts=ip_addr, arguments='-F') # add arguments here
+                result.append('Scanned 100 most common ports')
                 for host in nm.all_hosts():
                     for proto in nm[host].all_protocols():
                         lport = nm[host][proto].keys()
                         for port in lport:
                             result.append('Port:<b> ' + str(port) + ' </b> State: <b>' + nm[host][proto][port]['state'] + '</b>' )
             except:
-                print()
+                result.append('Error in nmap')
         except:
-            print()
-
+            result.append('Error in resolving url')
         return result
 
     def findIPaddr(url):
@@ -275,14 +294,14 @@ def results(request):
                 result.append('Updated Date:<b>' + str(d.updated_date) + '</b>')
             if(d.creation_date):
                 result.append('Creation Date:<b> ' + str(d.creation_date)+ '</b>')
-
             if(d.expiration_date):
-                result.append('Expiration Date:<b> ' + str(d.expiration_date)+ '</b>')
+                result.append('Expiration Date:<b> ' + str(d.expiration_date) + '</b>')
         except:
             result = 'Could not fetch'
 
         return result
 
+    #function to find heatbleed Vulnerability, uses the same pysslscan to parse the results
     def findHeartbeatVulnerability(url):
 
         result = ''
@@ -307,11 +326,12 @@ def results(request):
 
         return result
 
+    # function to find poodle Vulnerability, if list of protocols contains SSLv3, this results in True.
     def findPoodleVulnerability(url):
         result = ''
         try:
             protocols = findSupportedProtocols(url)
-            if(protocols.__contains__('SSL3')):
+            if(protocols.__contains__('SSL3')):             # add a condition for TLS1.0 to add that also a reason for poodle
                 result = 'Yes'
             else:
                 result = 'No'
@@ -321,22 +341,23 @@ def results(request):
 
 
 
-    entity = Entity('', '', '', '', '', '', '', '', '', '', '', '', '', '')
+    entity = Entity('', '', '', '', '', '', '', '', '', '', '', '', '', '')  #Initiate everything with null
 
     entity.name = url.replace("https://","")
     entity.status = checkStatus(url)
-    entity.validity = check_ssl(url)
-    entity.expiry = expiryDate(url)
-    entity.expiryDays = expiryDays(url)
-    entity.protocols = findSupportedProtocols(url)
-    entity.cipherSuites = findSupportedCipherSuites(url)
-    entity.reputation = checkReputation(url)
-    entity.TLDs = findOtherTLDs(url)
-    entity.openPorts = findOpenPorts(url)
-    entity.IPaddr = findIPaddr(url)
-    entity.owner = findOwner(url)
-    entity.heartbeat = findHeartbeatVulnerability(url)
-    entity.poodle = findPoodleVulnerability(url)
+    if(entity.status == "Live"):
+        entity.validity = check_ssl(url)
+        entity.expiry = expiryDate(url)
+        entity.expiryDays = expiryDays(url)
+        entity.protocols = findSupportedProtocols(url)
+        entity.cipherSuites = findSupportedCipherSuites(url)
+        entity.reputation = checkReputation(url)
+        entity.TLDs = findOtherTLDs(url)
+        entity.openPorts = findOpenPorts(url)
+        entity.IPaddr = findIPaddr(url)
+        entity.owner = findOwner(url)
+        entity.heartbeat = findHeartbeatVulnerability(url)
+        entity.poodle = findPoodleVulnerability(url)
 
 
 
