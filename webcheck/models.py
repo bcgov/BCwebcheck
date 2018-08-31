726961046458df2c086bd6336b6bab0010d6b9b9
Pranay Rai <40250685+pranay-rai@users.noreply.github.com>
2018-08-31 15:33:22 -0700

    update from last day.

--- webcheck/models.py
+++ webcheck/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django import forms
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 class URLForm(forms.Form):
     url = forms.CharField(label = 'Enter URL  ', max_length=100)
+
